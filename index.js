89b846f386b1ff3b1488f38340f9ecb6f17f53ae
Kurkov11 <152585755+Kurkov11@users.noreply.github.com>
2023-12-12 16:06:04 +0100

    initial-commit

---
+++ index.js
@@ -0,0 +1,40 @@
+document.getElementById('b1').onclick = function(){
+    if(document.getElementById('a1').style.display == 'block'){
+        document.getElementById('a1').style.display = 'none';
+        document.getElementById('b1').style.backgroundImage = 'url(assets/images/icon-plus.svg)';
+    }else{
+        document.getElementById('a1').style.display = 'block';
+        document.getElementById('a1').style.marginTop = '20px';
+        document.getElementById('b1').style.backgroundImage = 'url(assets/images/icon-minus.svg)';
+    }
+};
+document.getElementById('b2').onclick = function(){
+    if(document.getElementById('a2').style.display == 'block'){
+        document.getElementById('a2').style.display = 'none';
+        document.getElementById('b2').style.backgroundImage = 'url(assets/images/icon-plus.svg)';
+    }else{
+        document.getElementById('a2').style.display = 'block';
+        document.getElementById('a2').style.marginTop = '20px';
+        document.getElementById('b2').style.backgroundImage = 'url(assets/images/icon-minus.svg)';
+    }
+};
+document.getElementById('b3').onclick = function(){
+    if(document.getElementById('a3').style.display == 'block'){
+        document.getElementById('a3').style.display = 'none';
+        document.getElementById('b3').style.backgroundImage = 'url(assets/images/icon-plus.svg)';
+    }else{
+        document.getElementById('a3').style.display = 'block';
+        document.getElementById('a3').style.marginTop = '20px';
+        document.getElementById('b3').style.backgroundImage = 'url(assets/images/icon-minus.svg)';
+    }
+};
+document.getElementById('b4').onclick = function(){
+    if(document.getElementById('a4').style.display == 'block'){
+        document.getElementById('a4').style.display = 'none';
+        document.getElementById('b4').style.backgroundImage = 'url(assets/images/icon-plus.svg)';
+    }else{
+        document.getElementById('a4').style.display = 'block';
+        document.getElementById('a4').style.marginTop = '20px';
+        document.getElementById('b4').style.backgroundImage = 'url(assets/images/icon-minus.svg)';
+    }
+};
